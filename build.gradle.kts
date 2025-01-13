d8ddbe99005aca8eda659e84d5f4384794cc8497
Lexie Malina <tech@escape.systems>
2025-01-13 08:06:14 +0000

    Inital Hard-Fork Commit

---
+++ build.gradle.kts
@@ -0,0 +1,99 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    java // TODO java launcher tasks
+    id("io.papermc.paperweight.patcher") version "2.0.0-beta.13"
+}
+
+paperweight {
+    upstreams.register("folia") {
+        repo = github("PaperMC", "folia")
+        ref = providers.gradleProperty("foliaRef")
+
+        patchFile {
+            path = "folia-server/build.gradle.kts"
+            outputFile = file("cuneiform-server/build.gradle.kts")
+            patchFile = file("cuneiform-server/build.gradle.kts.patch")
+        }
+        patchFile {
+            path = "folia-api/build.gradle.kts"
+            outputFile = file("cuneiform-api/build.gradle.kts")
+            patchFile = file("cuneiform-api/build.gradle.kts.patch")
+        }
+        patchRepo("paperApi") {
+            upstreamPath = "paper-api"
+            patchesDir = file("cuneiform-api/paper-patches")
+            outputDir = file("paper-api")
+        }
+        patchRepo("paperApiGenerator") {
+            upstreamPath = "paper-api-generator"
+            patchesDir = file("cuneiform-api-generator/paper-patches")
+            outputDir = file("paper-api-generator")
+        }
+        patchDir("foliaApi") {
+            upstreamPath = "folia-api"
+            excludes = listOf("build.gradle.kts", "build.gradle.kts.patch", "paper-patches")
+            patchesDir = file("cuneiform-api/folia-patches")
+            outputDir = file("folia-api")
+        }
+//        patchDir("foliaServer") {
+//            upstreamPath = "folia-server"
+//            excludes = listOf("build.gradle.kts", "build.gradle.kts.patch", "paper-patches")
+//            patchesDir = file("cuneiform-server/folia-patches")
+//            outputDir = file("folia-server")
+//        }
+    }
+}
+
+val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
+
+subprojects {
+    apply(plugin = "java-library")
+    apply(plugin = "maven-publish")
+
+    extensions.configure<JavaPluginExtension> {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
+
+    repositories {
+        mavenCentral()
+        maven(paperMavenPublicUrl)
+    }
+
+    tasks.withType<AbstractArchiveTask>().configureEach {
+        isPreserveFileTimestamps = false
+        isReproducibleFileOrder = true
+    }
+    tasks.withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
+        options.release = 21
+        options.isFork = true
+    }
+    tasks.withType<Javadoc> {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    tasks.withType<ProcessResources> {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+    tasks.withType<Test> {
+        testLogging {
+            showStackTraces = true
+            exceptionFormat = TestExceptionFormat.FULL
+            events(TestLogEvent.STANDARD_OUT)
+        }
+    }
+
+    extensions.configure<PublishingExtension> {
+        repositories {
+            /*
+            maven("https://repo.papermc.io/repository/maven-snapshots/") {
+                name = "paperSnapshots"
+                credentials(PasswordCredentials::class)
+            }
+             */
+        }
+    }
+}
