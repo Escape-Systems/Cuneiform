0eff94efed99020e6e3c882d67b5b0dcd009a66f
Lexie Malina <alex@t-ch.net>
2026-02-28 11:08:42 -0600

    Well, we're ready for real this time! I swear.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
-    id("com.gradle.develocity") version("3.19")
+//    id("com.gradle.develocity") version("3.19")
 }
 
 rootProject.name = "cuneiform"
@@ -16,9 +16,9 @@ rootProject.name = "cuneiform"
 include("cuneiform-api")
 include("cuneiform-server")
 
-develocity {
-    buildScan {
-        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
-        termsOfUseAgree.set("yes")
-    }
-}
+//develocity {
+//    buildScan {
+//        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+//        termsOfUseAgree.set("yes")
+//    }
+//}
